61fb9057fcca1e9eb500ba29bf162ae35d209061
Brendan Bergen <bbergen@redhat.com>
2021-10-29 08:10:13 -0600

    Initial working workshop via learning-portal on 4.8

--- .workshop/settings.sh
+++ .workshop/settings.sh
@@ -1,7 +1,8 @@
-WORKSHOP_NAME=lab-markdown-sample
-WORKSHOP_IMAGE=quay.io/openshifthomeroom/lab-markdown-sample:1.10
-WORKSHOP_TITLE="Markdown Sample"
-WORKSHOP_DESCRIPTION="A sample workshop using Markdown"
+WORKSHOP_NAME=lab-helm-101
+WORKSHOP_IMAGE=quay.io/bbergen/lab-helm-101:0.1
+WORKSHOP_TITLE="Helm 101"
+WORKSHOP_DESCRIPTION="A workshop to introduce Helm basic concepts"
 RESOURCE_BUDGET=medium
 MAX_SESSION_AGE=3600
 IDLE_TIMEOUT=300
+SPAWNER_MODE="learning-portal"
